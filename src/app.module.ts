f80877c8745b82b4642b33a448c40c8939b4c502
ziangsza <ziangsza@gmail.com>
2025-02-02 00:39:52 -0800

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { ServiceModule } from './service/service.module';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [GameModule, ServiceModule],
+  imports: [GameModule, ServiceModule, PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
