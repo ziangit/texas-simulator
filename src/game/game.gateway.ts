0e3d056f8648f82bbe86131045fddc27cee2abaa
ziangsza <ziangsza@gmail.com>
2025-02-06 22:56:07 -0800

    socket sends message to all clients

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -4,20 +4,25 @@ import {
   SubscribeMessage,
   MessageBody,
   ConnectedSocket,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { GameService } from './game.service';
 import { Player } from './models/player.model';
 
 @WebSocketGateway({ cors: true })
 export class GameGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly gameService: GameService) { }
 
   @SubscribeMessage('test')
   handleTest(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
     console.log('Received test event:', data);
     // Emit a response back to the client
-    client.emit('testResponse', { message: 'Hello from NestJS' });
+    // client.emit('testResponse', { message: 'Hello from NestJS' });
+    this.server.emit('testResponse', { message: 'Hello from NestJS' })
   }
 
 
@@ -30,7 +35,9 @@ export class GameGateway {
       const newPlayer = new Player(data.name, client.id);
       this.gameService.addPlayer(newPlayer);
       // Broadcast updated game state to all connected clients
-      client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      // client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      this.server.emit('gameUpdate', this.gameService.getGameState());
+
       return { event: 'joined', data: newPlayer };
     } catch (error) {
       return { event: 'error', message: error.message };
@@ -42,8 +49,8 @@ export class GameGateway {
     try {
       this.gameService.startGame();
       // Emit to all connected clients, including the sender
-      client.broadcast.emit('gameUpdate', this.gameService.getGameState());
-      // client.server.emit('gameUpdate', this.gameService.getGameState());
+      // client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      this.server.emit('gameUpdate', this.gameService.getGameState());
       return { event: 'gameStarted', state: this.gameService.getGameState() };
     } catch (error) {
       return { event: 'error', message: error.message };
@@ -54,7 +61,9 @@ export class GameGateway {
   handleDealFlop(@ConnectedSocket() client: Socket) {
     try {
       this.gameService.dealFlop();
-      client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      // client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      this.server.emit('gameUpdate', this.gameService.getGameState());
+
       return { event: 'flopDealt', state: this.gameService.getGameState() };
     } catch (error) {
       return { event: 'error', message: error.message };
@@ -65,7 +74,9 @@ export class GameGateway {
   handleDealTurn(@ConnectedSocket() client: Socket) {
     try {
       this.gameService.dealTurn();
-      client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      // client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      this.server.emit('gameUpdate', this.gameService.getGameState());
+
       return { event: 'turnDealt', state: this.gameService.getGameState() };
     } catch (error) {
       return { event: 'error', message: error.message };
@@ -76,7 +87,8 @@ export class GameGateway {
   handleDealRiver(@ConnectedSocket() client: Socket) {
     try {
       this.gameService.dealRiver();
-      client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      // client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      this.server.emit('gameUpdate', this.gameService.getGameState());
       return { event: 'riverDealt', state: this.gameService.getGameState() };
     } catch (error) {
       return { event: 'error', message: error.message };
@@ -87,7 +99,8 @@ export class GameGateway {
   handleShowdown(@ConnectedSocket() client: Socket) {
     try {
       this.gameService.showdown();
-      client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      // client.broadcast.emit('gameUpdate', this.gameService.getGameState());
+      this.server.emit('gameUpdate', this.gameService.getGameState());
       return { event: 'showdown', state: this.gameService.getGameState() };
     } catch (error) {
       return { event: 'error', message: error.message };
